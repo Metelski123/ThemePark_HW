3e50a551a2a307707e7c904bff1f29421269654d
Jan Metelski <janekmetelski@hotmail.com>
2021-03-30 08:10:45 +0100

    Finished Extensions

---
+++ src/test/java/attractions/RollercoasterTest.java
@@ -0,0 +1,54 @@
+package attractions;
+
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+
+import static org.junit.Assert.assertEquals;
+
+public class RollercoasterTest {
+
+    RollerCoaster rollerCoaster;
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
+
+    @Before
+    public void setUp() {
+        rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor1 = new Visitor(18, 201, 10);
+        visitor2 = new Visitor(18, 140, 10);
+        visitor3 = new Visitor(11, 150, 10);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Blue Ridge", rollerCoaster.getName());
+    }
+
+    @Test
+    public void hasRating() {
+        assertEquals(10, rollerCoaster.getRating());
+    }
+
+    @Test
+    public void hasVisitCount() {
+        assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void checkIsAllowedToFalseHeight() {
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor2));
+    }
+
+    @Test
+    public void checkIsAllowedToFalseAge() {
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor3));
+    }
+
+    @Test
+    public void checkPriceForDouble() {
+        assertEquals(16.80, rollerCoaster.priceFor(visitor1), 0.00);
+    }
+}
+
